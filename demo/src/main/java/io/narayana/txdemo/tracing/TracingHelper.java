0568d7d119d1ac4a3954258f26ba8bd851c5b2a8
Miloslav Žežulka <mzezulka@localhost.localdomain>
2020-04-10 17:07:58 +0200

    Update service name so that it's not too much wordy.

--- demo/src/main/java/io/narayana/txdemo/tracing/TracingHelper.java
+++ demo/src/main/java/io/narayana/txdemo/tracing/TracingHelper.java
@@ -36,13 +36,18 @@ public class TracingHelper {
                 .withFlushInterval(1000)
                 .withMaxQueueSize(10000)
                 .withSender(senderConfig);
-        Builder bldr = new Configuration(System.getProperty("user.dir") + ":" + config.getProperty("tracer.component_name"))
+        Builder bldr = new Configuration(serviceName(config))
                 .withSampler(samplerConfig)
                 .withReporter(reporterConfig)
                 .getTracerBuilder();
         bldr.registerInjector(Format.Builtin.TEXT_MAP, TEXT_MAP_CODEC);
         bldr.registerExtractor(Format.Builtin.TEXT_MAP, TEXT_MAP_CODEC);
         return bldr.build();
+    }
+    
+    private static String serviceName(Properties config) {
+        String[] wflyPath = System.getProperty("user.dir").split("/");
+        return wflyPath[wflyPath.length-1] + ":" + config.getProperty("tracer.component_name");
     }
 
     static Properties loadConfig() {
